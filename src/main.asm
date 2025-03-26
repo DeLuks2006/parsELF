c3bf5a7a49dfa1e3681a59aadbd829ee96ae4a58
DeLuks2006 <dragojeviclk16@gmail.com>
2025-03-26 21:59:13 +0100

    Add cursed e_machine:ei_data table

--- src/main.asm
+++ src/main.asm
@@ -1,8 +1,8 @@
 ; TODO: 
-; - handle 32bit too
+; - compare ei_data and e_machine using lookup table or something
+; - handle 32bit
 ; - go over section headers
 ; - take big-endian into account
-; - compare ei_data and e_machine using lookup table or something
 ; ...
 ; takin some inspiration from travgm's parser: 
 ; - use string lookup tables instead of Hex-Only output
@@ -13,6 +13,7 @@
 %include "src/structs.asm"
 %include "src/std.asm"
 %include "src/hexdump.asm"
+%include "src/magic.asm"
 
 section   .bss
 ; VARIABLES ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ 
@@ -33,14 +34,187 @@ section   .rodata
   valid_magic db  0x7f, 0x45, 0x4c, 0x46
 
 ; MACHINE : ENDIAN TABLE
-;  em_table     db 0x00, 0x00, 0x01, 0x01, 0x02, 0x02,
-;                  0x03, 0x01, 0x04, 0x02, 0x05, 0x02, 
-;                  0x07, 0x01, 0x08, 0x02, 0x09, 0x02,
-;                  0x0A, 0x01, 0x0F, 0x01, 0x10, 0x02,
-;                  0x12, 0x02, 0x14, 0x02, 0x15, 0x02,
-;                  0x28, 0x01, 0x2a, 0x01, 0x2b, 0x02,
-;                  0x32, 0x01, 0x3e, 0x01, 0xb7, 0x01,
-;                  0xf3, 0x01,
+; HOLY MOTHER OF CURSED... polprog better throw this into a seperate file or find a better way to do this
+EM_TABLE	
+  db	EM_NONE,        	UNKNOWN_ENDIAN
+	db	EM_M32,         	BIG_ENDIAN
+	db	EM_SPARC,       	BIG_ENDIAN
+	db	EM_386,         	LITTLE_ENDIAN
+	db	EM_68K,         	BIG_ENDIAN
+	db	EM_88K,         	BIG_ENDIAN
+	db	EM_IAMCU,       	LITTLE_ENDIAN
+	db	EM_860,         	BIG_ENDIAN
+	db	EM_MIPS,        	BIG_ENDIAN
+	db	EM_S370,        	BIG_ENDIAN
+	db	EM_MIPS_RS3_LE,	  LITTLE_ENDIAN
+	db	EM_PARISC,	      BIG_ENDIAN
+	db	EM_VPP500,	      BIG_ENDIAN
+	db	EM_SPARC32PLUS,	  BIG_ENDIAN
+	db	EM_960,	          BIG_ENDIAN
+	db	EM_PPC,	          BIG_ENDIAN
+	db	EM_PPC64,	        BIG_ENDIAN
+	db	EM_S390,	        BIG_ENDIAN
+	db	EM_SPU,	          BIG_ENDIAN
+	db	EM_V800,	        BIG_ENDIAN
+	db	EM_FR20,	        BIG_ENDIAN
+	db	EM_RH32,	        BIG_ENDIAN
+	db	EM_RCE,	          BIG_ENDIAN
+	db	EM_ARM,	          LITTLE_ENDIAN
+	db	EM_FAKE_ALPHA,	  BIG_ENDIAN
+	db	EM_SH,	          BIG_ENDIAN
+	db	EM_SPARCV9,	      BIG_ENDIAN
+	db	EM_TRICORE,	      BIG_ENDIAN
+	db	EM_ARC,	          BIG_ENDIAN
+	db	EM_H8_300,	      BIG_ENDIAN
+	db	EM_H8_300H,	      BIG_ENDIAN
+	db	EM_H8S,	          BIG_ENDIAN
+	db	EM_H8_500,	      BIG_ENDIAN
+	db	EM_IA_64,	        BIG_ENDIAN
+	db	EM_MIPS_X,	      BIG_ENDIAN
+	db	EM_COLDFIRE,	    BIG_ENDIAN
+	db	EM_68HC12,	      BIG_ENDIAN
+	db	EM_MMA,	          BIG_ENDIAN
+	db	EM_PCP,	          BIG_ENDIAN
+	db	EM_NCPU,	        BIG_ENDIAN
+	db	EM_NDR1,	        BIG_ENDIAN
+	db	EM_STARCORE,	    BIG_ENDIAN
+	db	EM_ME16,     	    BIG_ENDIAN
+	db	EM_ST100,    	    BIG_ENDIAN
+	db	EM_TINYJ,    	    BIG_ENDIAN
+	db	EM_X86_64,      	LITTLE_ENDIAN
+	db	EM_PDSP,        	BIG_ENDIAN
+	db	EM_PDP10,       	BIG_ENDIAN
+	db	EM_PDP11,       	BIG_ENDIAN
+	db	EM_FX66,        	BIG_ENDIAN
+	db	EM_ST9PLUS,	      BIG_ENDIAN
+	db	EM_ST7,	          BIG_ENDIAN
+	db	EM_68HC16,	      BIG_ENDIAN
+	db	EM_68HC11,	      BIG_ENDIAN
+	db	EM_68HC08,	      BIG_ENDIAN
+	db	EM_68HC05,	      BIG_ENDIAN
+	db	EM_SVX,	          BIG_ENDIAN
+	db	EM_ST19,	        BIG_ENDIAN
+	db	EM_VAX,         	BIG_ENDIAN
+	db	EM_CRIS,        	LITTLE_ENDIAN
+	db	EM_JAVELIN,     	BIG_ENDIAN
+	db	EM_FIREPATH,    	BIG_ENDIAN
+	db	EM_ZSP,         	BIG_ENDIAN
+	db	EM_MMIX,        	BIG_ENDIAN
+	db	EM_HUANY,       	BIG_ENDIAN
+	db	EM_PRISM,       	LITTLE_ENDIAN
+	db	EM_AVR,         	BIG_ENDIAN
+	db	EM_FR30,    	    BIG_ENDIAN
+	db	EM_D10V,     	    BIG_ENDIAN
+	db	EM_D30V,     	    BIG_ENDIAN
+	db	EM_V850,     	    BIG_ENDIAN
+	db	EM_M32R,    	    BIG_ENDIAN
+	db	EM_MN10300,  	    BIG_ENDIAN
+	db	EM_MN10200,  	    BIG_ENDIAN
+	db	EM_PJ,          	BIG_ENDIAN
+	db	EM_OPENRISC,    	BIG_ENDIAN
+	db	EM_ARC_COMPACT, 	BIG_ENDIAN
+	db	EM_XTENSA,      	BIG_ENDIAN
+	db	EM_VIDEOCORE,   	BIG_ENDIAN
+	db	EM_TMM_GPP,     	BIG_ENDIAN
+	db	EM_NS32K,       	BIG_ENDIAN
+	db	EM_TPC,	          BIG_ENDIAN
+	db	EM_SNP1K,	        BIG_ENDIAN
+	db	EM_ST200,	        BIG_ENDIAN
+	db	EM_IP2K,	        BIG_ENDIAN
+	db	EM_MAX,	          BIG_ENDIAN
+	db	EM_CR,	          BIG_ENDIAN
+	db	EM_F2MC16,	      BIG_ENDIAN
+	db	EM_MSP430,	      BIG_ENDIAN
+	db	EM_BLACKFIN,	    BIG_ENDIAN
+	db	EM_SE_C33,	      BIG_ENDIAN
+	db	EM_SEP,	          BIG_ENDIAN
+	db	EM_ARCA,	        BIG_ENDIAN
+	db	EM_UNICORE,	      BIG_ENDIAN
+	db	EM_EXCESS,	      BIG_ENDIAN
+	db	EM_DXP,	          BIG_ENDIAN
+	db	EM_ALTERA_NIOS2,	BIG_ENDIAN
+	db	EM_CRX,         	BIG_ENDIAN
+	db	EM_XGATE,       	BIG_ENDIAN
+	db	EM_C166,        	BIG_ENDIAN
+	db	EM_M16C,        	BIG_ENDIAN
+	db	EM_DSPIC30F,    	BIG_ENDIAN
+	db	EM_CE,          	BIG_ENDIAN
+	db	EM_M32C,        	BIG_ENDIAN
+	db	EM_TSK3000,     	BIG_ENDIAN
+	db	EM_RS08,        	BIG_ENDIAN
+	db	EM_SHARC,       	BIG_ENDIAN
+	db	EM_ECOG2,       	BIG_ENDIAN
+	db	EM_SCORE7,      	BIG_ENDIAN
+	db	EM_DSP24,	        BIG_ENDIAN
+	db	EM_VIDEOCORE3,	  BIG_ENDIAN
+	db	EM_LATTICEMICO32, BIG_ENDIAN
+	db	EM_SE_C17,	      BIG_ENDIAN
+	db	EM_TI_C6000,	    BIG_ENDIAN
+	db	EM_TI_C2000,	    BIG_ENDIAN
+	db	EM_TI_C5500,	    BIG_ENDIAN
+	db	EM_TI_ARP32,	    BIG_ENDIAN
+	db	EM_TI_PRU,	      BIG_ENDIAN
+	db	EM_MMDSP_PLUS,	  BIG_ENDIAN
+	db	EM_CYPRESS_M8C,	  BIG_ENDIAN
+	db	EM_R32C,	        BIG_ENDIAN
+	db	EM_TRIMEDIA,	    BIG_ENDIAN
+	db	EM_QDSP6,	        BIG_ENDIAN
+	db	EM_8051,	        BIG_ENDIAN
+	db	EM_STXP7X,	      BIG_ENDIAN
+	db	EM_NDS32,	        BIG_ENDIAN
+	db	EM_ECOG1X,	      BIG_ENDIAN
+	db	EM_MAXQ30,	      BIG_ENDIAN
+	db	EM_XIMO16,	      BIG_ENDIAN
+	db	EM_MANIK,	        BIG_ENDIAN
+	db	EM_CRAYNV2,	      BIG_ENDIAN
+	db	EM_RX,	          BIG_ENDIAN
+	db	EM_METAG,   	    BIG_ENDIAN
+	db	EM_MCST_ELBRUS,	  BIG_ENDIAN
+	db	EM_ECOG16,      	BIG_ENDIAN
+	db	EM_CR16,	        BIG_ENDIAN
+	db	EM_ETPU,        	BIG_ENDIAN
+	db	EM_SLE9X,       	BIG_ENDIAN
+	db	EM_L10M,        	BIG_ENDIAN
+	db	EM_K10M,        	BIG_ENDIAN
+	db	EM_AARCH64,     	BIG_ENDIAN
+	db	EM_AVR32,       	BIG_ENDIAN
+	db	EM_STM8,        	BIG_ENDIAN
+	db	EM_TILE64,      	BIG_ENDIAN
+	db	EM_TILEPRO,     	BIG_ENDIAN
+	db	EM_MICROBLAZE,  	BIG_ENDIAN
+	db	EM_CUDA,        	BIG_ENDIAN
+	db	EM_TILEGX,      	BIG_ENDIAN
+	db	EM_CLOUDSHIELD, 	BIG_ENDIAN
+	db	EM_COREA_1ST,	    BIG_ENDIAN
+	db	EM_COREA_2ND,	    BIG_ENDIAN
+	db	EM_ARC_COMPACT2,	BIG_ENDIAN
+	db	EM_OPEN8,	        BIG_ENDIAN
+	db	EM_RL78,	        BIG_ENDIAN
+	db	EM_VIDEOCORE5,    BIG_ENDIAN
+	db	EM_78KOR,	        BIG_ENDIAN
+	db	EM_56800EX,	      BIG_ENDIAN
+	db	EM_BA1,         	BIG_ENDIAN
+	db	EM_BA2,	          BIG_ENDIAN
+	db	EM_XCORE,	        BIG_ENDIAN
+	db	EM_MCHP_PIC,	    BIG_ENDIAN
+	db	EM_KM32,	        BIG_ENDIAN
+	db	EM_KMX32,	        BIG_ENDIAN
+	db	EM_EMX16,	        BIG_ENDIAN
+	db	EM_EMX8,	        BIG_ENDIAN
+	db	EM_KVARC,	        BIG_ENDIAN
+	db	EM_CDP,	          BIG_ENDIAN
+	db	EM_COGE,	        BIG_ENDIAN
+	db	EM_COOL,	        BIG_ENDIAN
+	db	EM_NORC,	        BIG_ENDIAN
+	db	EM_CSR_KALIMBA,	  BIG_ENDIAN
+	db	EM_Z80,	          BIG_ENDIAN
+	db	EM_VISIUM,	      BIG_ENDIAN
+	db	EM_FT32,	        BIG_ENDIAN
+	db	EM_MOXIE,	        BIG_ENDIAN
+	db	EM_AMDGPU,  	    BIG_ENDIAN
+	db	EM_RISCV,	        BIG_ENDIAN
+	db	EM_BPF,	          BIG_ENDIAN
+	db	EM_CSKY,	        BIG_ENDIAN
 
 ; STRINGS ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ 
   parself       db  0x0A, "           [ ParsELF - A Mini-ELF Parser ]          ", 0x0A, 0x00
